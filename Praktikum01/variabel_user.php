4a0941bae74faa4317e8aada94de9870e9370429
Balqisfarah29 <0110124147@student.nurulfikri.ac.id>
2025-04-24 12:06:14 +0700

    praktikum01

---
+++ Praktikum01/variabel_user.php
@@ -0,0 +1,11 @@
+<?php
+//mendefinisikan variabel
+$nama = "Balqis Farah";
+$umur = 19;
+$berat = 50.00;
+
+echo "Nama : " . $nama;
+echo "<br/>Umur : " . $umur . " Tahun";
+echo "<br/>Berat : " . $berat . " kg";
+
+?>
