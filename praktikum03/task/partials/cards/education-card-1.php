267389a61c96657bf1a2266957cd4e7fde04893b
Balqisfarah29 <0110124147@student.nurulfikri.ac.id>
2025-05-05 17:50:40 +0700

    Praktikum03

---
+++ praktikum03/task/partials/cards/education-card-1.php
@@ -0,0 +1,20 @@
+<div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <div class="text-secondary fw-bolder mb-2">2020 - 2023</div>
+                                                <div class="mb-2">
+                                                    <div class="small fw-bolder">Man 1 Bogor</div>
+                                                    <div class="small text-muted">Cibinong</div>
+                                                </div>
+                                                <div class="fst-italic">
+                                                    <div class="small text-muted"></div>
+                                                    <div class="small text-muted"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8"><div>mantappp</div></div>
+                                    </div>
+                                </div>
+                            </div>
---
+++ praktikum03/task/partials/cards/project-card-2.php
@@ -0,0 +1,11 @@
+<div class="card overflow-hidden shadow rounded-4 border-0">
+                                <div class="card-body p-0">
+                                    <div class="d-flex align-items-center">
+                                        <div class="p-5">
+                                            <h2 class="fw-bolder">Pemweb 1</h2>
+                                            <p></p>
+                                        </div>
+                                        <img class="img-PEMWEB1.png" src="task/assets/PEMWEB1.png" alt="..." />
+                                    </div>
+                                </div>
+</div>
