39bdd04b90dadfc22c4de534e3d2a52615682aff
Balqisfarah29 <0110124147@student.nurulfikri.ac.id>
2025-06-24 16:37:17 +0700

    edit file create-user dan list-user

--- views/list-user.php
+++ views/list-user.php
@@ -64,7 +64,7 @@ $users = User::get();
                 </div>
                 <div class="sb-sidenav-footer">
                     <div class="small">Logged in as:</div>
-                    Cindi Novianti
+                    Balqis Farah
                 </div>
             </nav>
         </div>
